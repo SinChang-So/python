b6d633babef6802537225c705f2fb7a0fa2f9a4a
SinChang-So <71159995+SinChang-So@users.noreply.github.com>
2022-03-17 13:32:45 +0900

    Create 0317.py

---
+++ 20220317/0317.py
@@ -0,0 +1,53 @@
+#001
+print("Hello World")
+
+#002
+print("Mary's cosmetics")
+
+#003
+print('신씨가 소리질렀다. "도둑이야" ')
+
+#004
+print("C:\Windows")
+
+#005
+print("안녕하세요. \n만나서\t\t반갑습니다.")  #\n 줄바꿈 \t 탭
+
+#051
+movie_rank = ['닥터 스트레인지','스플릿','럭키']
+print(movie_rank)
+
+#052
+movie_rank.append('배트맨')
+print(movie_rank)
+
+#053
+movie_rank.insert(1,'슈퍼맨')
+print(movie_rank)
+
+#058
+nums =[1,2,3,4,5]
+i= sum(nums)
+print(i)
+
+#059
+cook = ["피자", "김밥", "만두", "양념치킨", "족발", "피자", "김치만두", "쫄면", "소시지", "라면", "팥빙수", "김치전"]
+k=len(cook)
+print(k)
+
+#060
+j=len(nums)
+print(i/j)
+
+#102
+print(3==5) #False 3과 5는 같을수 없으니 False 가 뜸
+
+#103
+print(3<5) #True 3이 5보다 작은건 맞음으로 True가 뜬다.
+
+#104
+x =4
+print(1<x<5)  #True x는 1보다 크고 5보다 작음으로 True가 뜬다.
+
+#105
+print((3==3) and (4!=3)) #True 3과3은 같고 4와 3이 같지 않음으로 True가 뜬다.
